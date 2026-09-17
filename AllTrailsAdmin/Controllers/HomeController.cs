eec513a9256fe2643ef00b1a431bd91f277d5678
Michael Trumbull <michael.trumbull@nscc.ca>
2026-09-17 14:30:26 -0300

    Starting to look at controllers and routing.

--- AllTrailsAdmin/Controllers/HomeController.cs
+++ AllTrailsAdmin/Controllers/HomeController.cs
@@ -6,20 +6,36 @@ namespace AllTrailsAdmin.Controllers
 {
     public class HomeController : Controller
     {
+        // Constructor
+        public HomeController()
+        {
+            Console.WriteLine("Constructor is called!");
+        }
+
+        // Action Method /Index
         public IActionResult Index()
         {
+            Console.WriteLine("Hello from the Home/Index.");
+
             return View();
         }
 
+        // Action Method /Privacy
         public IActionResult Privacy()
         {
+            Console.WriteLine("Hello from the Home/Privacy.");
+
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
+
+
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
